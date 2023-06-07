7f12a5eea23700794e2f245ee1e3aacec2ee6869
hyeontaek-Cha <mygusxor12368@gmail.com>
2023-06-07 13:38:07 +0900

    test

--- bash_script/test.sh
+++ bash_script/test.sh
@@ -1,19 +1,11 @@
 #! /bin/bash
 
-yum -y install maven git
+update-alternatives --config java
 
-
-wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-yum -y install jenkins
-
-
-systemctl start jenkins && systemctl enable jenkins
-
-
-wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-
-yum install -y ca-certificates
-
-yum install -y java-11-openjdk jenkins
-yum install jenkins
+/usr/bin/expect <<EOF
+  set prompt "#"
+  expect {
+    "Selection*" { send "3\r"; exp_continue }
+    $prompt
+}
+EOF
