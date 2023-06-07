d73f0e79c08a779f2803ae04efe4c40b3fbd6ccd
hyeontaek-Cha <mygusxor12368@gmail.com>
2023-06-07 12:08:16 +0900

    push

--- bash_script/install_prometheus.sh
+++ bash_script/install_prometheus.sh
@@ -3,16 +3,24 @@
 # install metric server
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.6.3/components.yaml
 
-# Get the current deployment YAML
 CURRENT_YAML=$(kubectl get deployment.apps/metrics-server -n kube-system -o yaml)
 
-# Add the '--kubelet-insecure-tls' option to the command line arguments
-NEW_YAML=$(echo "$CURRENT_YAML" | sed 's/args:$/args:\n        - --kubelet-insecure-tls/g')
+NEW_YAML=$(echo "$CURRENT_YAML" | sed '/- --secure-port=4443/a\        - --kubelet-insecure-tls')
+
+NEW_YAML=$(echo "$NEW_YAML" | sed '/dnsPolicy: ClusterFirst/a\        hostNetwork: true')
 
 echo "$NEW_YAML" | kubectl apply -f -
 
+sleep 5s
+
+pod_name=$(kubectl get pod -n kube-system -l app=metrics-server -o jsonpath='{.items[0].metadata.name}')
+kubectl delete pod -n kube-system $pod_name --grace-period=0 --force
+
+pod_name=$(kubectl get pod -n kube-system -l app=kube-state-metrics -o jsonpath='{.items[0].metadata.name}')
+kubectl delete pod -n kube-system $pod_name --grace-period=0 --force
+
 # waiting pod loading
-sleep 1m
+sleep 20s
 
 # create namespace
 kubectl create namespace monitoring
@@ -24,6 +32,17 @@ git clone https://github.com/hali-linux/my-prometheus-grafana.git
 
 cd my-prometheus-grafana
 
+
+# replace prometheus-cluster-role.yaml
+tmp_file=$(mktemp)
+
+sed "s/$search_string/$replace_string/g" "$file_path" > "$tmp_file"
+
+mv "$tmp_file" "$file_path"
+
+rm "$tmp_file"
+
+#install prometheus
 kubectl apply -f prometheus-cluster-role.yaml
 sleep 3s
 
