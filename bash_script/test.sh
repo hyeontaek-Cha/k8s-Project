8a770eb7438c12c6b59dca69af06bbbdf7ebc53d
hyeontaek-Cha <mygusxor12368@gmail.com>
2023-06-07 10:22:59 +0900

    push

--- bash_script/test.sh
+++ bash_script/test.sh
@@ -1,3 +1,19 @@
 #! /bin/bash
 
-echo test
+yum -y install maven git
+
+
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+yum -y install jenkins
+
+
+systemctl start jenkins && systemctl enable jenkins
+
+
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+
+yum install -y ca-certificates
+
+yum install -y java-11-openjdk jenkins
+yum install jenkins
